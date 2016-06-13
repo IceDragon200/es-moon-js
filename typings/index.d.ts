d5030f959355b71a654dcbd42c89213bbd92e861
Corey Powell <mistdragon100@gmail.com>
2016-06-13 00:05:49 -0500

    Added keypress typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,4 @@
+/// <reference path="globals/keypress/index.d.ts" />
 /// <reference path="globals/lodash/index.d.ts" />
 /// <reference path="globals/pixi.js/index.d.ts" />
 /// <reference path="globals/wolfy87-eventemitter/index.d.ts" />
