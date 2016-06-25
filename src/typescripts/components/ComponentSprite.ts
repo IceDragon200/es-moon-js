9d2e4ab9e82f930d0fbe4e3934947dd69aebcfbb
Corey Powell <mistdragon100@gmail.com>
2016-06-25 18:30:43 -0500

    Removed position attribute in ComponentSprite
    Sprites now rely on ComponentPosition to achieve the same effect

--- src/typescripts/components/ComponentSprite.ts
+++ src/typescripts/components/ComponentSprite.ts
@@ -4,7 +4,6 @@ import Component from "ecs/Component";
 export default class ComponentSprite extends Component {
 	// texture handle name
 	public texture: string;
-	public position = new Vec2(0, 0);
 	public opacity: number = 1.0;
 	public _sprite: PIXI.Sprite;
 	public _requireRefresh: boolean = false;
