1ab989c91482b872d2a6d884c0baa00502a02a6b
Corey Powell <mistdragon100@gmail.com>
2016-06-25 18:28:33 -0500

    Removed old keypress typing

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,4 +1,3 @@
-/// <reference path="globals/keypress/index.d.ts" />
 /// <reference path="globals/lodash/index.d.ts" />
 /// <reference path="globals/pixi.js/index.d.ts" />
 /// <reference path="globals/wolfy87-eventemitter/index.d.ts" />
